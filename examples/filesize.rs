9e47fe0b7b19c7e365aab7e08523dd20c9d6a882
Thomas Hurst <tom@hur.st>
2020-03-19 04:25:53 +0000

    Update example

--- examples/filesize.rs
+++ examples/filesize.rs
@@ -1,21 +1,32 @@
-use filesize;
 use std::io;
+use std::path::Path;
+
+use filesize::PathExt;
+
+fn display_path<P: AsRef<Path>>(path: P) -> io::Result<()> {
+    let path = path.as_ref();
+    let meta = path.symlink_metadata()?;
+    let logical = meta.len();
+    let physical = path.size_on_disk_fast(&meta)?;
+
+    println!(
+        "{:>9} {:>9} {:>9.2}x {}",
+        logical,
+        physical,
+        physical as f64 / logical as f64,
+        path.display()
+    );
+
+    Ok(())
+}
 
 fn main() -> io::Result<()> {
-    println!("{:>9} {:>9} {:>9} {}", "Logical", "Physical", "Ratio", "Path");
+    println!("{:>9} {:>9} {:>9} Path", "Logical", "Physical", "Ratio");
 
     for path in std::env::args_os().skip(1) {
-        let meta = std::fs::symlink_metadata(&path)?;
-        let logical = meta.len();
-        let physical = filesize::file_real_size_fast(&path, &meta)?;
-
-        println!(
-            "{:>9} {:>9} {:>9.2}x {}",
-            logical,
-            physical,
-            physical as f64 / logical as f64,
-            path.to_string_lossy()
-        );
+        if let Err(e) = display_path(&path) {
+            eprintln!("{}: {}", path.to_string_lossy(), e);
+        }
     }
 
     Ok(())
